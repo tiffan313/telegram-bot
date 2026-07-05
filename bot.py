3ac15fd0bad68682814cb86b153bcf8275e499bb
tiffan313 <54986582+tiffan313@users.noreply.github.com>
2026-07-05 17:16:15 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,28 +6,26 @@ TOKEN = os.getenv("BOT_TOKEN")
 
 keyboard = ReplyKeyboardMarkup(
     [
-        ["سلام"],
-        ["سلام"],
-        ["سلام"],
-        ["سلام"],
-        ["سلام"]
+        ["قرآن 📖", "حدیث 📚"],
+        ["جستجو 🔍"]
     ],
     resize_keyboard=True
 )
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "یکی از دکمه‌ها را انتخاب کن:",
+        "سلام 👋 یکی از گزینه‌ها رو انتخاب کن:",
         reply_markup=keyboard
     )
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام 👋")
+    await update.message.reply_text("در حال توسعه 👷‍♂️")
 
-app = Application.builder().token(TOKEN).build()
+if __name__ == "__main__":
+    app = Application.builder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
 
-print("Bot is running...")
-app.run_polling()
+    print("Bot is running...")
+    app.run_polling()
